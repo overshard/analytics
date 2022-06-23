4832136bf260a23eba56239e12a81f693bd4778c
Isaac Bythewood <isaac@bythewood.me>
2022-06-23 23:11:44 +0000

    Change custom_cards to allow null and blank

--- properties/models.py
+++ properties/models.py
@@ -15,7 +15,7 @@ class Property(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='properties')
     name = models.CharField(max_length=255)
-    custom_cards = models.JSONField(default=list)
+    custom_cards = models.JSONField(default=list, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True, editable=False)
     updated_at = models.DateTimeField(auto_now=True, editable=False)
     is_protected = models.BooleanField(default=False, editable=False)
